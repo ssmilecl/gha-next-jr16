65c739ad9aa1c3db6140f05b455b3c01949b367b
WilliamDJR <william.wj.dong@gmail.com>
2025-06-28 10:34:49 +1000

    Changed the app page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ export const metadata = {
 export default function Page() {
   return (
     <>
-        <h1>App Router</h1>
-        <h2 style={{ textAlign: "center", color: "red" }}>Changed something</h2>
+        <h1 style= {{ textAlign: "center", color: "blue" }}>Nextjs Demo</h1>
+        <hr />	
+        <h2 style={{ textAlign: "center", color: "red" }}>Change something here</h2>
     </>
   );
 }
